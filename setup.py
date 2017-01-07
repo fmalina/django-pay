f8013bc7a29c99069751f32999686de818051a59
Frantisek Malina <frantisek.malina@interoute.com>
2017-01-07 20:28:50 +0000

    Updated setup

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-# -*- encoding: utf-8 -*-
 from setuptools import setup, find_packages
-import pay as app
+import pay
 
 setup(
     name="pay",
-    version=app.__version__,
+    version=pay.__version__,
     description='Django payment credit and debit card Realex/Paypal subscriptions',
     long_description=open('README.rst').read(),
     license='BSD License',
     platforms=['OS Independent'],
     keywords='realex, paypal, credit card, django, app, subscriptions',
-    author='fmalina',
+    author='F. Malina',
     author_email='fmalina@gmail.com',
     url="https://github.com/fmalina/django-pay",
     packages=find_packages(),
