aff47602c9170938e39643041af169d7a448e2ce
F. Malina <fmalina@pm.me>
2017-06-14 16:34:54 +0100

    Added a screen to manage subscription where users can turn recurring billing on or off.

--- pay/urls.py
+++ pay/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^paycards$', views.paycards, name='paycards'),
     url(r'^add_card$', views.add_card, name='add_card'),
     url(r'^receipts$', views.receipts, name='receipts'),
+    url(r'^subscription$', views.subscription, name='subscription'),
     url(r'^receipt/(?P<id>[0-9]+)$', views.receipt, name='receipt'),
     url(r'^voiding/(?P<id>[0-9]+)$', views.void, name='void'),
     url(r'^paypal$', views.paypal, name='paypal'),
