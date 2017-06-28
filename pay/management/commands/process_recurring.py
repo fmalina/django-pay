9d8c591aa1b68e7502e321d0473f2703cf309df7
Frantisek Malina <frantisek.malina@interoute.com>
2017-06-28 13:29:20 +0100

    Added basic output to recurring charge processing.

--- pay/management/commands/process_recurring.py
+++ pay/management/commands/process_recurring.py
@@ -10,18 +10,23 @@ def charge(s):
     last_payment = s.user.payment_set.filter(complete=True, method='cc').last()
     card = last_payment.card
     amount = get_amount(s.plan)
+    print()
+    print(s.user, s.plan, amount)
     p, review_needed = auth_payment(card, amount)
     if p.complete:
         s.expires = s.expires + timedelta(days=s.plan)
         s.save()
+        print('Success, charged', p.amount)
+    else:
+        print('Failure', p.cardreceipt.details)
 
 
 class Command(BaseCommand):
-    """ Daily batch recurring charge processing.
+    """ Daily batch processing recurring charges.
     """
     def handle(self, *args, **kwargs):
         now = datetime.now()
         yesterday = now - timedelta(hours=24)
-
+        print('Processing recurring charges:', now)
         for s in Subscription.objects.filter(expires__range=(yesterday, now), recurring=True):
             charge(s)
