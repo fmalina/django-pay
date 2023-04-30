521a16b6d81eb55f12e3a1b1164d5f925068c2e3
F. Malina <fmalina@pm.me>
2023-04-30 14:01:15 +0200

    Removed stray migration dependency

--- pay/migrations/0001_initial.py
+++ pay/migrations/0001_initial.py
@@ -11,7 +11,6 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0012_alter_user_first_name_max_length'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
